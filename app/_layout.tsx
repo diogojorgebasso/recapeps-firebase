cc97faa7dda53ca2f1d19bf2fc5d6111883d91c6
Diogo Jorge Basso <65865529+diogojorgebasso@users.noreply.github.com>
2024-10-07 02:34:34 +0200

    refactor: update package configurations and Google services file paths
    Signed-off-by: Diogo Jorge Basso <65865529+diogojorgebasso@users.noreply.github.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,33 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+
+  const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+    console.log('onAuthStateChanged', user);
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
   });
 
   useEffect(() => {
